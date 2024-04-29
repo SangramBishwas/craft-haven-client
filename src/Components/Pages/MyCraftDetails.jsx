9218dddb7d5ad782929ca34f5bfa5b930deb4d13
Sangram Bishwas <info.sangram2003@gmail.com>
2024-04-29 10:06:48 +0600

    Apply delete method on my crafts data successfully

--- src/Components/Pages/MyCraftDetails.jsx
+++ src/Components/Pages/MyCraftDetails.jsx
@@ -1,9 +1,35 @@
 import PropTypes from 'prop-types';
 import { FaStarHalfAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyCraftDetails = ({ craft }) => {
-    const { _id, name, image, price, rating, stock, customization } = craft
+    const { _id, name, image, price, rating, stock, customization } = craft;
+    const handleDelete = (id) => {
+        console.log('delete this craft', id);
+        fetch(`http://localhost:5000/craft/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((data) => {
+            console.log(data)
+            if (data.deletedCount > 0) {
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
+    }
     return (
         <div className="card flex-col lg:flex-row card-side bg-base-100 shadow-xl">
             <figure className='lg:w-1/2'><img className='lg:h-full ' src={image} alt="Movie" /></figure>
@@ -24,8 +50,8 @@ const MyCraftDetails = ({ craft }) => {
                     <button className="btn btn-sm btn-primary">
                         <Link to={`/update/${_id}`}>Update</Link>
                     </button>
-                    <button className="btn btn-sm btn-primary">Delete</button>
-                    
+                    <button onClick={() => handleDelete(_id)} className="btn btn-sm bg-red-600 hover:bg-red-700 text-white">Delete</button>
+
                 </div>
             </div>
         </div>
